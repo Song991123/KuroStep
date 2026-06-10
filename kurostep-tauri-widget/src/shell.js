ddae0f6eb778a0fa7d21540edf77856754fbdcf2
Song991123 <sjh11235678@gmail.com>
2026-06-10 18:51:07 +0900

    Patch vanilla widget partial refresh bugs

--- kurostep-tauri-widget/src/shell.js
+++ kurostep-tauri-widget/src/shell.js
@@ -23,7 +23,7 @@ function contentUrl() {
   const url = new URL(view === "paw" ? "paw.html" : "", baseUrl);
   url.searchParams.set("embedded", "1");
   url.searchParams.set("shell", "tauri");
-  url.searchParams.set("v", "20260610-22");
+  url.searchParams.set("v", "20260610-23");
   return url.toString();
 }
 
