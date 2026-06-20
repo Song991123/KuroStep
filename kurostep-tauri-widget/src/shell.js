ff41de2afd8a6f66834522e22e7c89e4f084045d
Song991123 <sjh11235678@gmail.com>
2026-06-21 03:07:15 +0900

    chore(release): prepare v0.1.4 lyric cache bust

--- kurostep-tauri-widget/src/shell.js
+++ kurostep-tauri-widget/src/shell.js
@@ -38,7 +38,7 @@ function contentUrl() {
   url.searchParams.set("view", view);
   url.searchParams.set("embedded", "1");
   url.searchParams.set("shell", "tauri");
-  url.searchParams.set("v", "20260621-v013");
+  url.searchParams.set("v", "20260621-v014");
   trustedContentOrigin = url.origin;
   return url.toString();
 }
